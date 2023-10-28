cfb58c6bd9872d0d4fde1c8426ce73f7e39f5e09
Rashik Tuladhar <rashiktuladhar13@gmail.com>
2023-10-28 21:01:17 +0545

    .

---
+++ src/components/videoPopup/VideoPopUp.jsx
@@ -0,0 +1,26 @@
+import "./videoPopUp.scss";
+
+const VideoPopUp = ({ show, setShow, videoId, setVideoId }) => {
+  const hidePopup = () => {
+    setShow(false);
+    setVideoId(null);
+  };
+  return (
+    <div className={`videoPopup ${show ? "visible" : ""}`}>
+      <div className="opacityLayer" onClick={hidePopup}></div>
+      <div className="videoPlayer">
+        <span className="closeBtn" onClick={hidePopup}>
+          Close
+        </span>
+        <iframe
+          width={"100%"}
+          height={"100%"}
+          src={`https://www.youtube.com/embed/${videoId}`}
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPopUp;
